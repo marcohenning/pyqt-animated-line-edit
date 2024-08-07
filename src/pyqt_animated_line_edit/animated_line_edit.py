eaf5f545d6871c9cbba4c94233572d25d170c344
Marco Henning <marcohnngpersonal@gmail.com>
2024-08-07 17:13:11 +0200

    Start implementing basic functionality

--- src/pyqt_animated_line_edit/animated_line_edit.py
+++ src/pyqt_animated_line_edit/animated_line_edit.py
@@ -7,7 +7,29 @@ class AnimatedLineEdit(QLineEdit):
 
     def __init__(self, placeholderText, parent=None):
         super(AnimatedLineEdit, self).__init__(parent)
+
         self.placeholderText = placeholderText
+        self.font = QFont('Times', 10)
+        self.setContentsMargins(0, 5, 0, 0)
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        painter = QPainter(self)
+        painter.setFont(self.font)
+        painter.setPen(QColor(100, 100, 100))
+
+        if not self.hasFocus():
+            painter.drawText(10, 25, self.placeholderText)
+        else:
+            painter.drawText(10, 5, self.placeholderText)
+
+    def focusInEvent(self, event):
+        super().focusInEvent(event)
+        print('focusInEvent')
+
+    def focusOutEvent(self, event):
+        super().focusOutEvent(event)
+        print('focusOutEvent')
 
     def setPlaceholderText(self, placeholderText):
         self.placeholderText = placeholderText
