6804a99144bd0145e04c240324ffd5814eb796af
Marco Henning <marcohnngpersonal@gmail.com>
2024-08-08 17:46:50 +0200

    Start implementing getters and setters

--- src/pyqt_animated_line_edit/animated_line_edit.py
+++ src/pyqt_animated_line_edit/animated_line_edit.py
@@ -9,6 +9,9 @@ class AnimatedLineEdit(QLineEdit):
         super(AnimatedLineEdit, self).__init__(parent)
 
         self.placeholderText = placeholderText
+        self.backgroundColor = '#FFFFFF'
+        self.borderWidth = 1
+        self.borderRadius = 0
         self.fontInner = QFont('Times', 11)
         self.fontOuter = QFont('Times', 9)
         self.fontCurrent = QFont(self.fontInner.family(), self.fontInner.pointSize())
@@ -18,8 +21,9 @@ class AnimatedLineEdit(QLineEdit):
         self.textOuter = self.fontMetricsOuter.boundingRect(self.placeholderText)
         self.topOffset = int((self.textOuter.height() - 1) / 2)
         self.setContentsMargins(0, self.topOffset, 0, 0)
-        self.positionInner = QPoint(10, self.height() - int((self.height() - self.textInner.height()) / 2))
-        self.positionOuter = QPoint(10, int(self.textOuter.height() - self.topOffset / 2))
+        self.placeholderTextStart = max(15, self.borderRadius + 10)
+        self.positionInner = QPoint(self.placeholderTextStart, self.height() - int((self.height() - self.textInner.height()) / 2))
+        self.positionOuter = QPoint(self.placeholderTextStart, int(self.textOuter.height() - self.topOffset / 2))
         self.positionCurrent = QPoint(self.positionInner.x(), self.positionInner.y())
         self.isPlaceholderInside = True
 
@@ -65,8 +69,9 @@ class AnimatedLineEdit(QLineEdit):
         painter.setFont(self.fontCurrent)
 
         if not self.isPlaceholderInside:
-            painter.setPen(QColor(255, 255, 255))
-            painter.drawLine(QPoint(5, self.topOffset), QPoint(self.textOuter.width() + 15, self.topOffset))
+            painter.setPen(QColor(self.backgroundColor))
+            for i in range(self.borderWidth):
+                painter.drawLine(QPoint(self.placeholderTextStart - 5, self.topOffset + i), QPoint(self.placeholderTextStart + self.textOuter.width() + 5, self.topOffset + i))
 
         if not self.hasFocus() and not self.text():
             painter.setPen(QColor(100, 100, 100))
@@ -93,3 +98,34 @@ class AnimatedLineEdit(QLineEdit):
 
     def setPlaceholderText(self, placeholderText):
         self.placeholderText = placeholderText
+
+    def getBackgroundColor(self):
+        return self.backgroundColor
+
+    def setBackgroundColor(self, color):
+        self.backgroundColor = color.name()
+
+    def getBorderWidth(self):
+        return self.borderWidth
+
+    def setBorderWidth(self, borderWidth):
+        self.borderWidth = borderWidth
+
+    def getBorderRadius(self):
+        return self.borderRadius
+
+    def setBorderRadius(self, borderRadius):
+        self.borderRadius = borderRadius
+
+    def getFontInner(self):
+        return self.fontInner
+
+    def setFontInner(self, font):
+        self.fontInner = font
+
+    def getFontOuter(self):
+        return self.fontOuter
+
+    def setFontOuter(self, font):
+        self.fontOuter = font
+        self.fontCurrent = QFont(self.fontInner.family(), self.fontInner.pointSize())
